19bd1677a6636654f02079ee5ec6e849899669ad
wayn3h0 <me@wayn3h0.com>
2014-11-06 15:07:04 +0800

    add file dependency

---
+++ dependency/file/file.go
@@ -0,0 +1,43 @@
+package file
+
+import (
+	"encoding/gob"
+	"github.com/landjur/go-caching/dependency"
+	"os"
+	"time"
+)
+
+// register the type for gob encoding.
+func init() {
+	gob.Register(&file{})
+}
+
+// New returns a new file caching dependency.
+func New(filePath string) dependency.Dependency {
+	var lastModifiedTime time.Time
+	fi, err := os.Stat(filePath)
+	if err != nil {
+		lastModifiedTime = time.Now()
+	} else {
+		lastModifiedTime = fi.ModTime()
+	}
+
+	return &file{
+		Path:             filePath,
+		LastModifiedTime: lastModifiedTime,
+	}
+}
+
+type file struct {
+	Path             string
+	LastModifiedTime time.Time
+}
+
+func (this file) HasChanged() bool {
+	fi, err := os.Stat(this.Path)
+	if err != nil {
+		return true
+	}
+
+	return fi.ModTime().After(this.LastModifiedTime)
+}
